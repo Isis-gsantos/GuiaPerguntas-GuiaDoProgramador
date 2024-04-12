1ef49147a46c7df6d7829eaf58636063d2711346
Isis Gonçalves Santos <isissantos8425@gmail.com>
2024-04-12 10:40:12 -0300

    adicionando sequelize e criando uma tabela

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database"); //esse arquivo por sua vez exporta o objeto de conexão
+const perguntaModel = require("./database/Pergunta");
+
+//Database
+connection
+    .authenticate()
+//o authenticate vai tentar logar no mysql e após ele tentar autenticar 2 coisas podem acontecer: ou ele vai conseguir ou vai acontecer um erro. Caso ele conseguir se conectar vai chamar a função .then(() => {}) que é chamada apenas quando a autentificação foi um sucesso e caso o erro aconteça vai chamar a função .catch((erro) => {})
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+    //portanto, vou fazer a autentificação com a conexão, se for um sucesso executa o then e se der erro vai executar o catch -> PROMISE
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
